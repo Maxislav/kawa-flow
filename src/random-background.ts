bc8de0d5720381b3f1dbdeeb64beff2c47581309
Maxim Lipatov <maximmalyshyn@gmail.com>
2022-02-04 22:54:56 +0200

    moto flow

--- src/random-background.ts
+++ src/random-background.ts
@@ -3,12 +3,18 @@ const getRandomInt = (min: number, max: number) => {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
+let i = 1;
 
 setInterval(() => {
     const el1: HTMLImageElement = document.querySelector('.main-image');
     const el2: HTMLImageElement = document.querySelector('.main-image-fade');
-    const i = getRandomInt(1, 7);
+    let ni = 0;
+
+    do {
+        ni = getRandomInt(1, 7);
+    }while (i === ni)
+    i = ni;
+    //  const i = getRandomInt(1, 7);
     el2.src = `assets/img/fade-in-out/${i}.jpg`;
     el2.style.opacity = '1';
 
